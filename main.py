bbabe6d03321801b12a7c58bcb4bf6cd303dbe40
Ankit Chaudhary <ankit.chaudhary@tietoevry.com>
2022-01-14 00:28:12 +0530

    First Commit

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ name_of_unit = 'seconds'
 
 
 def days_to_units(value):
-    return value * to_seconds
+    if value > 0:  # conditional check
+        return value * to_seconds
+    else:
+        return 'invalid Input : enter only positive value.'
 
 
 user_input = input("Please Type  :\n")
 
 x = days_to_units(int(user_input))
 print(x)
+print(type(x))
+print(hash(user_input))
